f5395100c9a8c735804c61f5e1b070602d3b3a82
Noixas <acmrz@outlook.com>
2019-05-28 12:13:55 +0200

    Cleaner folder structure, reading and writing files not gonna work since everything was moved to different folder, make sure to edit the path properly

--- Credentials.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-def get_youtube_credential():
-    with open('Private/credentials.json') as creds:    
-        credentials = json.load(creds)
-    return credentials['youtube_api']['DEVELOPER_KEY']
-print(get_youtube_credential())
